55a50fc7b7fb5d25835755201421fbb99f243a97
Aleksey Salamatin <asalamatin640@gmail.com>
2024-03-31 14:46:22 +0500

    added a timer and centered the window

--- text_quest/Page2.xaml.cs
+++ text_quest/Page2.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace text_quest
 {
@@ -20,9 +21,27 @@ namespace text_quest
     /// </summary>
     public partial class Page2 : Page
     {
+        DispatcherTimer _timer;
+        TimeSpan _time;
         public Page2()
         {
             InitializeComponent();
+
+            _time = TimeSpan.FromSeconds(10);
+
+            _timer = new DispatcherTimer(new TimeSpan(0, 0, 1), DispatcherPriority.Normal, delegate
+            {
+                tbTime.Text = _time.ToString("ss");
+                if (_time == TimeSpan.Zero)
+                {
+                    _timer.Stop();
+                    CounterTransfer.counter += 0;
+                    NavigationService.Navigate(new Page3());
+                }
+                _time = _time.Add(TimeSpan.FromSeconds(-1));
+            }, Application.Current.Dispatcher);
+
+            _timer.Start();
         }
         public class CounterTransfer
         {
@@ -30,16 +49,19 @@ namespace text_quest
         }
         private void nextFromOne(object sender, RoutedEventArgs e)
         {
+            _timer.Stop();
             CounterTransfer.counter += 1;
             NavigationService.Navigate(new Page3());
         }
         private void nextFromTwo(object sender, RoutedEventArgs e)
         {
+            _timer.Stop();
             CounterTransfer.counter += 2;
             NavigationService.Navigate(new Page3());
         }
         private void nextFromThree(object sender, RoutedEventArgs e)
         {
+            _timer.Stop();
             CounterTransfer.counter += 3;
             NavigationService.Navigate(new Page3());
         }
--- text_quest/Page3.xaml.cs
+++ text_quest/Page3.xaml.cs
@@ -12,7 +12,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 using static text_quest.Page2;
+
+
 
 namespace text_quest
 {
@@ -21,27 +24,45 @@ namespace text_quest
     /// </summary>
     public partial class Page3 : Page
     {
+        DispatcherTimer _timer;
+        TimeSpan _time;
         int counter = CounterTransfer.counter;
         public Page3()
         {
             InitializeComponent();
+
+            _time = TimeSpan.FromSeconds(10);
+
+            _timer = new DispatcherTimer(new TimeSpan(0, 0, 1), DispatcherPriority.Normal, delegate
+            {
+                tbTime.Text = _time.ToString("ss");
+                if (_time == TimeSpan.Zero)
+                {
+                    _timer.Stop();
+                    CounterTransfer.counter += 0;
+                    NavigationService.Navigate(new Page4());
+                }
+                _time = _time.Add(TimeSpan.FromSeconds(-1));
+            }, Application.Current.Dispatcher);
+
+            _timer.Start();
         }
         private void nextFromOne(object sender, RoutedEventArgs e)
         {
             CounterTransfer.counter += 1;
-
+            _timer.Stop();
             NavigationService.Navigate(new Page4());
         }
         private void nextFromTwo(object sender, RoutedEventArgs e)
         {
             CounterTransfer.counter += 2;
-
+            _timer.Stop();
             NavigationService.Navigate(new Page4());
         }
         private void nextFromThree(object sender, RoutedEventArgs e)
         {
             CounterTransfer.counter += 3;
-
+            _timer.Stop();
             NavigationService.Navigate(new Page4());
         }
     }
